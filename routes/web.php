66683021fa5439ac9f43b6e379bf9340f672f4db
Danie1Arias <daniel.arias@estudiants.urv.cat>
2025-11-07 10:36:10 +0100

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('welcome');
+})->name('home');
+
+Route::get('/second', function () {
+    return Inertia::render('second');
+})->name('home');
+
